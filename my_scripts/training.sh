ca58a0ce8d459a6157aa5cf34f9b8faa270b8815
Achyut Boggaram <achyut.boggaram@clobotics.com>
2018-10-29 11:35:31 -0700

    adding my scripts

---
+++ my_scripts/training.sh
@@ -0,0 +1,18 @@
+python /home/caffe/achu/facenet/src/train_softmax.py \
+	--logs_base_dir ~/achu/logs/facenet/ \
+	--models_base_dir ~/achu/models/facenet/ \
+	--data_dir /home/caffe/Pictures/CCNA_data/images/ccna_cropped_data \
+	--image_size 256 \
+	--model_def models.inception_resnet_v1 \
+	--optimizer ADAM \
+	--pretrained_model /home/caffe/achu/models/facenet/20181023-164011/model-20181023-164011.ckpt-200 \
+	--learning_rate 0.0005 \
+	--max_nrof_epochs 100 \
+	--batch_size 32 \
+	--keep_probability 0.4 \
+	--random_flip \
+	--use_fixed_image_standardization \
+	--weight_decay 5e-4 \
+	--embedding_size 512 \
+	--validation_set_split_ratio 0.01 \
+	--validate_every_n_epochs 5
